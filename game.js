20e6aea3f9c74a5ff4442acbb2d99f4a30d75c0e
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2024-03-13 00:21:02 +0800

    make animation and sound when next sequence occur

--- game.js
+++ game.js
@@ -1,1 +1,45 @@
+const buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
 
+function nextSequence() {
+  // create random number for the sequence
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  let randomChosenColours = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColours);
+
+  $("#" + randomChosenColours)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  var audio = new Audio("./sounds/" + randomChosenColours + ".mp3");
+
+  audio.play();
+}
+
+// function makeSound(key) {
+//   switch (key) {
+//     case "green":
+//       let greenAudio = new Audio("./sounds/green.mp3");
+//       greenAudio.play();
+//       break;
+//     case "blue":
+//       let blueAudio = new Audio("./sounds/blue.mp3");
+//       blueAudio.play();
+//       break;
+//     case "red":
+//       let redAudio = new Audio("./sounds/red.mp3");
+//       redAudio.play();
+//       break;
+//     case "yellow":
+//       let yellowAudio = new Audio("./sounds/yellow.mp3");
+//       yellowAudio.play();
+//       break;
+
+//     default:
+//       console.log(key);
+//   }
+// }
