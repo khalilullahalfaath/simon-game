9bc1f04f34b0cfbc1cb1d808c9cdec7b4a1c0cd0
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2024-03-13 09:30:08 +0800

    add levels

--- game.js
+++ game.js
@@ -1,7 +1,33 @@
 const buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
+
+let started = false;
+
+let level = 0;
+
+$(document).on("keydown", function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").on("click", function () {
+  let userChosenColour = this.attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+
+  animatePress(userChosenColour);
+});
 
 function nextSequence() {
+  // update level
+  level += 1;
+  $("#level-title").text("Level " + level);
+
   // create random number for the sequence
   let randomNumber = Math.floor(Math.random() * 3) + 1;
 
@@ -15,31 +41,20 @@ function nextSequence() {
     .fadeOut(100)
     .fadeIn(100);
 
-  var audio = new Audio("./sounds/" + randomChosenColours + ".mp3");
+  playSound(randomChosenColours);
+}
 
+function playSound(name) {
+  var audio = new Audio("./sounds/" + name + ".mp3");
   audio.play();
 }
 
-// function makeSound(key) {
-//   switch (key) {
-//     case "green":
-//       let greenAudio = new Audio("./sounds/green.mp3");
-//       greenAudio.play();
-//       break;
-//     case "blue":
-//       let blueAudio = new Audio("./sounds/blue.mp3");
-//       blueAudio.play();
-//       break;
-//     case "red":
-//       let redAudio = new Audio("./sounds/red.mp3");
-//       redAudio.play();
-//       break;
-//     case "yellow":
-//       let yellowAudio = new Audio("./sounds/yellow.mp3");
-//       yellowAudio.play();
-//       break;
+function animatePress(currentColour) {
+  // add animation
+  $("#" + currentColour).addClass("pressed");
 
-//     default:
-//       console.log(key);
-//   }
-// }
+  // remove animation after 100ms delay
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
